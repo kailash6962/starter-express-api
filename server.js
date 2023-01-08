4eeebe0bda8fa70a0fad936d5863b1dde8dd140c
kailashk6962 <kailashk6962@gmail.com>
2023-01-08 09:02:46 +0530

    auth check new 3

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const app = express();
 
 //db connection
 mongoose.connect(process.env.DATABASE)
-.then(() => console.log('DB Connected'))
+.then(() => console.log('DB Connected 2'))
 .catch((err) => console.log('DB Connection Error: ', err));
+console.log('Auth JS Check 1');
 
 //middleware
 app.use(cors());
 app.use(morgan("prod"));
 app.use(express.json());
-console.log('Auth JS Check 1');
 
 //route middleware
 readdirSync('./routes').map((r) => 
